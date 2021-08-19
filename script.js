21988ecbee405e696ca7b47bb3cf171fe85cf526
Chriskybizz <67864176+Chriskybizz@users.noreply.github.com>
2021-08-19 23:38:23 +0100

    Add files via upload
    slider baubles, auto slide and returns to one when you get to the end

--- script.js
+++ script.js
@@ -3,27 +3,50 @@ let imgNumber = 1
 // check image and move function
 const slideCheck = () => {
     if (imgNumber === 1) {
-        document.getElementById("dogImg").src = "dogOne.webp"
+        document.getElementById("dogImg").src = "dogOne.webp";
+        document.getElementById("trackerOne").style.backgroundColor="white"
+        document.getElementById("trackerTwo").style.backgroundColor="red"
+        document.getElementById("trackerThree").style.backgroundColor="red"
+        document.getElementById("trackerFour").style.backgroundColor="red"
+
     }
     if (imgNumber === 2) {
         document.getElementById("dogImg").src = "dogTwo.webp"
+        document.getElementById("trackerOne").style.backgroundColor="red"
+        document.getElementById("trackerTwo").style.backgroundColor="white"
+        document.getElementById("trackerThree").style.backgroundColor="red"
+        document.getElementById("trackerFour").style.backgroundColor="red"
     }
     if (imgNumber === 3) {
         document.getElementById("dogImg").src = "dogThree.webp"
+        document.getElementById("trackerOne").style.backgroundColor="red"
+        document.getElementById("trackerTwo").style.backgroundColor="red"
+        document.getElementById("trackerThree").style.backgroundColor="white"
+        document.getElementById("trackerFour").style.backgroundColor="red"
     }
     if (imgNumber === 4) {
         document.getElementById("dogImg").src = "dogFour.webp"
+        document.getElementById("trackerOne").style.backgroundColor="red"
+        document.getElementById("trackerTwo").style.backgroundColor="red"
+        document.getElementById("trackerThree").style.backgroundColor="red"
+        document.getElementById("trackerFour").style.backgroundColor="white"
 
     }
 }
 
 // interval slide function
 const slide = () => {
-    if (imgNumber >= 0 && imgNumber < 4) {
+    if (imgNumber >= 1 && imgNumber < 4) {
         imgNumber++;
         slideCheck()
     } else {
-        imgNumber = 0;
+        imgNumber = 1;
+        document.getElementById("dogImg").src = "dogOne.webp";
+        document.getElementById("trackerOne").style.backgroundColor="white"    
+        document.getElementById("trackerTwo").style.backgroundColor="red"
+        document.getElementById("trackerThree").style.backgroundColor="red"
+        document.getElementById("trackerFour").style.backgroundColor="red"    
+
     }
 }
 
@@ -34,9 +57,12 @@ const right = () => {
     } else { imgNumber = 0 }
 }
 const left = () => {
-    if (imgNumber > 0) {
+    if (imgNumber > 1) {
         imgNumber--
         slideCheck()
+    } else {
+        imgNumber = 4;
+        document.getElementById("dogImg").src = "dogFour.webp";
     }
 }
 
